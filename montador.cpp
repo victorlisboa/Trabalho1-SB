f62d41249a71721c8ac8ffb95d25afdb67d5864b
Victor Hugo <victorhugof.lisboa@gmail.com>
2023-09-30 17:52:32 -0300

    Lendo o arquivo

---
+++ montador.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main(int arg, char *argv[]) {
+
+    if(arg < 2) {
+        cout << "Voce deve incluir o nome do programa .asm para compilacao!";
+        return 0;
+    }
+    
+    cout << "Conteudo do programa " << argv[1] << ":\n";
+    
+    string text;
+    ifstream file(argv[1]);
+
+    while(getline(file, text)) {
+        cout << text << '\n';
+    }
+
+    file.close();
+
+    return 0;
+}
